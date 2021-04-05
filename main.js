25ea040fa4dc8de7ba7937f4650f901671de2783
jeremybarbosa <jbarbosa1430@gmail.com>
2021-04-05 08:28:28 -0500

    hue

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// =============================================================================
 
 // Remember that our list is called `todos`, and it's in `todos.js`. It's not in this file, nor is it being `require`-d in, but it's available globally because its file is loaded in BEFORE this one in `index.html`.
 
@@ -6,7 +5,7 @@
 
 // Doing our code under its pre-written comment in this way will self-document your code and, more importantly, make it easier to follow along with the assignment!
 
-// Remember that each function below is a helper function or an event listener function that runs when the user interacts with our page. So we should NOT be calling any functions or doing anythinmg in the global scope. Just functions functions functions!
+// Remember that each function below is a helper function or an event listener function that runs when the user interacts with our page. So we should NOT be calling any functions or doing anything in the global scope. Just functions functions functions!
 // (With one big exception, which you'll see in the comments when we get there.)
 
 // And now: code away!
@@ -15,51 +14,63 @@
 
 // A function that, given a todo object, adds an item to our todo list array.
 
-
+const addAnObject = function addObject (insertObject){
+  todos.push(insertObject); 
+}
 
 // A function that removes an item at a given index from our todo list array. You can use splice!
 
-
-
-// A function that takes in a todo object and displays it on the DOM. This is a pretty big function, so we'll walk through the different parts of it.
-const printTodo = function(todo) {
-  // Use `document.createElement` to make an <li>.
-
-  
-  // Set its text (preferably using `.innerText`) to be our given object's text field. Check out what a todo object looks like in `todos.js` if you need to!
-
-
-  // Give our new li a `todo-item` class using `classList`. This will allow us to style it later if we want.
-
-
-  // Give our new li an id that is the object's id. This is so that we have a matching relationship between todo _html elements_ and their corresponding _array objects_. Now we'll be able to find the corresponding array object when they click to toggle the completeness on a DOM element.
-
-  
-  // Give the li a `complete` class if the todo object indicates it was complete already. (Again, check the `todos.js` to see what the objects look like!)
-
-
-  // Query the todo list <ol> and store it in a variable
-
-
-  // Append the li we made to the ol as the last child using `.appendChild`. If this isn't working for you, check what is being appended to what!
-
+const removeAnObject = function removeObject(){
+  todos.splice();
 }
 
-
+// A function that takes in a todo object and displays it on the DOM. This is a pretty big function, so we'll walk through the different parts of it.
+const printTodo = function(obj) {
+  // Use `document.createElement` to make an <li>.
+  const childLi = document.createElement('li')
+  // Set its text (preferably using `.innerText`) to be our given object's text field. Check out what a todo object looks like in `todos.js` if you need to!
+  childLi.innerText = obj.text;
+  console.log(childLi);
+  // Give our new li a `todo-item` class using `classList`. This will allow us to style it later if we want.
+  childLi.classList.add('todo-item')
+  // Give our new li an id that is the object's id. This is so that we have a matching relationship between todo _html elements_ and their corresponding _array objects_. Now we'll be able to find the corresponding array object when they click to toggle the completeness on a DOM element.
+  const getID = obj.id;
+  childLi.setAttribute('id', getID);
+  
+  // Give the li a `complete` class if the todo object indicates it was complete already. (Again, check the `todos.js` to see what the objects look like!)
+  if (obj.complete === true){
+  childLi.classList.add("complete"); 
+  }
+  // Query the todo list <ol> and store it in a variable
+  const list = document.querySelector(".todo-list");
+  // Append the li we made to the ol as the last child using `.appendChild`. If this isn't working for you, check what is being appended to what!
+  list.appendChild(childLi)
+}
 // A function that print ALL todos. It should loop through our todos array and call the above print-one-todo function on each one.
-
-
-
+function printAllToDos (obj){
+  for (obj of todos){
+    printTodo(obj)
+  }
+}
 // Now here in the global code, call the above function, so our todos array gets printed out on page load (which is when global code is run). This is the only time we're calling a function ourselves; the rest is event listeners and helper functions that run when the user interacts with the DOM!
-
+printAllToDos()
 
 
 // A function that clears all todos from the DOM. This is a great helper function for refreshing our todos.
 // Test it in the console and see if your list disappears!
-
+function clearAll (){
+  const list = document.querySelectorAll(".todo-item")
+  for (const item of list){
+    item.remove()
+  }
+}
 
 
 // A function that refreshes our page by calling each of the two above functions. Since printing all todos onto the DOM is based on our todos array, if we make a change to our todos array, we can simply call this function, which will make our DOM match our todos array by simply clearing the page and repopulating it according to our todos' new state.
+const refreshToDos = function(){
+  clearAll();
+  printAllToDos();
+}
 
 
 
@@ -71,13 +82,29 @@ Let's wire it all together. Add an event listener for the add todo button that w
 4. Pass the object as well to your adding todos function to put it on the DOM.
 5. Stretch goal: remove all text from the input box. Try adding multiple todos without this first, you'll see why we should do it!
 */
+const addButton = document.querySelector('.add-todo');
+addButton.addEventListener ("click", function (){
+  let textBox = document.querySelector(".todo-input")
+  let newTodO =  {
+    text: textBox.value,
+    complete: false,
+    priority: 2,
+    id: todos.length,
+  };
+
+  addAnObject(newTodO);
+  printTodo(newTodO);
+})
 
 
 
 /* 
- Run over to the HTML and add a button for CLEAR TODOS or REMOVE TODOS or some such, giving it a class or id of your choice. Now let's wire up that button, giving it a click event listener that clears all todos from the DOM (we have a function for that!) and removes all todo objects from the todos array as well.
+Run over to the HTML and add a button for CLEAR TODOS or REMOVE TODOS or some such, giving it a class or id of your choice. Now let's wire up that button, giving it a click event listener that clears all todos from the DOM (we have a function for that!) and removes all todo objects from the todos array as well.
 */
-
-
+const clearButton = document.querySelector(".clear-todo");
+clearButton.addEventListener("click", function(){
+  todos.splice(0, todos.length);
+  refreshToDos();
+})
 
 // And you're DONE with the best interface we've written yet for a todos app!
